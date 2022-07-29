5d84f97598ba8b476238be376a3e9e2f2e50aa7f
Ahmedxd1 <86264396+Ahmedxd1@users.noreply.github.com>
2022-07-29 19:49:17 +0300

    ahmedaddNav

---
+++ app/src/main/java/com/ahmed/ahmed/RegiseterActivity.java
@@ -0,0 +1,13 @@
+package com.ahmed.ahmed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegiseterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiseter);
+    }
+}
